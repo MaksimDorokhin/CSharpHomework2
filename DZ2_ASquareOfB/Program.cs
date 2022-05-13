3cbc6af4bb50478e610691b54ba9d144fe47b95b
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-13 22:30:52 +0300

    Added DZ2_ASquareOfB

---
+++ DZ2_ASquareOfB/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}!");
+}
+else if (b == a * a)
+{
+    Console.WriteLine($"Число {b} является квадратом числа {a}!");
+}
+else
+{
+    Console.WriteLine($"Ни одно из введенных чисел не является квадратом второго!");
+}
